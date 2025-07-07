5646d73419673dc910fe1b4c38b64b4ce32a9ff5
Carl Erik Gerlach <carlerik.gerlach@schueler.max-eyth-schule.de>
2025-07-07 22:21:32 +0200

    Files from die source directory are copied to the destination directory

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import schedule as schedule
 import shutil
 
 
-src_path = r"D:\testdict\\"
-dst_path = r"D:\test saves folder\test"
+src_path = r"D:\Alles mögliche"
+dst_path = r"\\MYCLOUDEX2ULTRA\Carl\Saves\saves-alles-mögliche\saveFile"
+
+
+
 
 
 def copyDirecory():
